4230d0f95238384730e18cee84f34395a0412f9f
dreamzack <dreamzack@gmail.com>
2014-01-22 10:53:44 -0600

    create all methods for assignment1

--- src/com/example/assignment1/MainActivity.java
+++ src/com/example/assignment1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assignment1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,8 +10,59 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.i("MainActivity", "I'm in onCreate");
 		setContentView(R.layout.activity_main);
+		
 	}
+	
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.i("MainActivity", "I'm in onDestroy");
+	}
+
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.i("MainActivity", "I'm in onPause");
+	}
+
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.i("MainActivity", "I'm in onRestart");
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.i("MainActivity", "I'm in onResume");
+	}
+
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.i("MainActivity", "I'm in onStart");
+	}
+
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.i("MainActivity", "I'm in onStop");
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
